dfadfde048a4e484fc14f487be048c2bcb641ac0
unknown <kindermolina98@gmail.com>
2025-10-20 21:06:05 -0300

    image is being produced;

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 ### What is this?
 
 I am just proving image visualization with ASCII characters
+
+### setup
+
+with python a local server could be deployed
+
+```
+python -m http.server 8000
+```
--- index.js
+++ index.js
@@ -7,15 +7,20 @@ function preload(){
 }
 
 function setup(){
-    createCanvas(400,400);
+    createCanvas(600,400);
+    textFont('monospace', 8);
+    textAlign(CENTER, CENTER);
+    noLoop();
 }
 
 function draw() {
     background(220);
-    //image(gloria, 0, 0, width, height);
+    gloria.resize(80, 0);
+    gloria.loadPixels();
+    
     let w = width / gloria.width;
     let h = height / gloria.height;
-    gloria.loadPixels();
+    //image(gloria, 0, 0, width, height);
     for (let i=0;i<gloria.width;i++) {
         for (let j=0;j<gloria.height;j++) {
             const pixelIndex = (i + j * gloria.width) * 4;
@@ -23,11 +28,16 @@ function draw() {
             const g = gloria.pixels[pixelIndex + 1];
             const b = gloria.pixels[pixelIndex + 2];
 
-            let avg = (r+g+b)/3;
-            noStroke();
-            fill(r,g,b);
+            let avg = (r + g + b)/3;
+            let len = density.length;
+            const charIdx = floor(map(avg, 0, 255, 0, len - 1));
+            const c = density.charAt(charIdx);
+            //noStroke();
+            //fill(r,g,b);
             //square(i * w, j * h, w);
-            text('G', i+w, j+h);
+            //text('G', i+w, j+h);
+            fill(255);
+            text(c, i * w, j * h);
         }
     }
 }
